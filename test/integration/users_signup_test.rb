7e8869d36c8c4d87782d52393de6ea17cd74a1a4
Eckchart <thesynner03@gmail.com>
2026-02-19 23:03:15 +0200

    Better Invalid Signup Users test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,13 +18,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # the error count (should be 4 for this test)) appeared.
     assert_select 'div#error_explanation'
 
-    # Check that the number of errors for this test is 4 (Rails
-    # also wraps the labels with the `field_with_errors` class so
-    # we need to check the count to be 4 * 2 = 8)
-    assert_select 'div.field_with_errors', count: 4 * 2
-
-    # Idk if we can test more than this.. seems too brittle to
-    # check for string in the actual error messages or anything
-    # like that.
+    assert_select 'div.field_with_errors input#user_name'
+    assert_select 'div.field_with_errors input#user_email'
+    assert_select 'div.field_with_errors input#user_password'
+    assert_select 'div.field_with_errors input#user_password_confirmation'
   end
 end
