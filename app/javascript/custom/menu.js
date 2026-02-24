8e4142a31ac4972a61861911d4d10b33c330b2f3
Eckchart <thesynner03@gmail.com>
2026-02-24 20:38:50 +0200

    Add mobile styling to header and footer

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,11 +1,27 @@
 // Menu manipulation
 
+// Adds a toggle listener.
+function addToggleListener(selected_id, menu_id, toggle_class) {
+    let selected_element = document.querySelector(`#${selected_id}`);
+
+    // In the case of adding an event listener for the element
+    // with id = `account`:
+    // If we are NOT logged in, `selected_element` will be null,
+    // since there was no element rendered with id = `account`, so
+    // we first need to check if `selected_element` actually holds
+    // the html object with id = `account` (i.e., isn't null) before
+    // adding an event listener to it.
+    if (selected_element) {
+        selected_element.addEventListener("click", function(event) {
+            event.preventDefault();
+            let menu = document.querySelector(`#${menu_id}`);
+            menu.classList.toggle(toggle_class);
+        });
+    }
+}
+
 // Add toggle listeners to listen for clicks.
 document.addEventListener("turbo:load", function() {
-    let account = document.querySelector("#account");
-    account.addEventListener("click", function(event) {
-        event.preventDefault();
-        let menu = document.querySelector("#dropdown-menu");
-        menu.classList.toggle("active");
-    })
+    addToggleListener("account", "dropdown-menu", "active");
+    addToggleListener("hamburger", "navbar-menu", "collapse");
 });
