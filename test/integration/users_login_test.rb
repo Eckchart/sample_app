99e2d632994ec62ba830749d24b883a6da0434fe
Eckchart <thesynner03@gmail.com>
2026-02-24 22:13:09 +0200

    Add successful login test and fix unsuccessful login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+  def setup
+    # From fixture '/test/fixtures/users.yml'.
+    @user = users(:michael)
+  end
+  
+  test "login with valid email / invalid password" do
     get login_path
     assert_template 'sessions/new'
     post(login_path, params: {
       session: {
-        email: "",
-        password: ""
+        email: "michael@example.com",
+        password: "invalid"
       }
     })
     assert_response :unprocessable_entity
@@ -15,5 +20,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    # ASSUME ALL FIXTURE USERS HAVE THE SAME
+    # PASSWORD (i.e. 'password').
+    post(login_path, params: {
+      session: {
+        email: @user.email,
+        password: 'password'
+      }
+    })
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
