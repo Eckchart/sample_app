7d656b5f6f5ef88c9faa3d202b1e7f349e1afeb2
Eckchart <thesynner03@gmail.com>
2026-03-01 19:55:43 +0200

    Add destroy and index actions tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -57,4 +57,41 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch(user_path(@other_user), params: {
+      user: {
+        password: "password",
+        password_confirmation: "password",
+        admin: true
+      }
+    })
+    # After the PATCH request executes, the instance VARIABLE
+    # `@other_user` will still have its `admin` attribute set
+    # to FALSE *EVEN IF* we permit the `admin` attribute to be
+    # edited, in our `user_params` method; but, in the DB,
+    # the record that `@other_user` holds does have its `admin`
+    # attribute set to `true`.
+    
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference "User.count" do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
 end
