5d23376607e36594a9cbdefad7980d6e6b55280f
Eckchart <thesynner03@gmail.com>
2026-03-07 23:35:01 +0200

    Add Micropost delete functionality

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   # create new micropost (post request)
   def create
@@ -14,10 +15,23 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted!"
+
+    # For a multitude of reasons, `request.referrer` (whose
+    # source is the browser's HTTP "Referer" header, and which may
+    # be used by this `redirect_back_or_to` method) may be nil,
+    # in which case we redirect to `root_url` by default.
+    redirect_back_or_to(root_url, status: :see_other)
   end
 
   private
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to(root_url, status: :see_other) if @micropost.nil?
     end
 end
