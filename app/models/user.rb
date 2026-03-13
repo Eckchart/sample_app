1c8425ac2d2bc433ecf204c57373ec3e9aae864a
Eckchart <thesynner03@gmail.com>
2026-03-13 17:34:23 +0200

    Add Relationship model and User/Relationship associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_secure_password
 
   before_save { self.email.downcase! }
@@ -22,5 +30,22 @@ class User < ApplicationRecord
   # Defines a proto-feed.
   def feed
     Micropost.where("user_id = ?", self.id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    # `<<` appends `other_user` to the end of the `following` ""array""
+    # (TECHNICALLY NOT AN ARRAY BUT IT ACTS LIKE AN ARRAY).
+    self.following << other_user unless self == other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    self.following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    self.following.include?(other_user)
   end
 end
---
+++ db/migrate/20260312142533_create_relationships.rb
@@ -0,0 +1,17 @@
+class CreateRelationships < ActiveRecord::Migration[7.0]
+  def change
+    create_table :relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+
+    # The table's primary key will basically be
+    # the pair (follower_id, followed_id), which
+    # makes sense!
+    add_index :relationships, [:follower_id, :followed_id], unique: true 
+  end
+end
